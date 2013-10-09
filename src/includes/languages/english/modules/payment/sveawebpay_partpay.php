37e69821b5d5995d464debb400180d2a26e2b2ad
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2013-10-09 14:49:34 +0200

    typos

--- src/includes/languages/english/modules/payment/sveawebpay_partpay.php
+++ src/includes/languages/english/modules/payment/sveawebpay_partpay.php
@@ -7,7 +7,7 @@ Version 4.0
 define('MODULE_PAYMENT_SWPPARTPAY_TEXT_TITLE','SVEA Part Payment');
 define('MODULE_PAYMENT_SWPPARTPAY_TEXT_DESCRIPTION','SveaWebPay Webservice Part Payment Solution - ver. 4.0');
 
-define('ERROR_ALLOWED_CURRENCIES_NOT_DEFINED','One or more of the allowed currencies are not defined. This must be enabled in order to use the SweaWebPay Hosted Solution. Log in to your admin panel, and ensure that all currencies listed as allowed in the paymend module exists, and that the correct exchange rates are set.');
+define('ERROR_ALLOWED_CURRENCIES_NOT_DEFINED','One or more of the allowed currencies are not defined. This must be enabled in order to use the SweaWebPay Hosted Solution. Log in to your admin panel, and ensure that all currencies listed as allowed in the payment module exists, and that the correct exchange rates are set.');
 define('ERROR_DEFAULT_CURRENCY_NOT_ALLOWED','The default currency is not among those listed as allowed. Log in to your admin panel, and ensure that the default currency is in the allowed list in the payment module.');  
 define('ERROR_MESSAGE_PAYMENT_FAILED','Payment Failed.');  
 
