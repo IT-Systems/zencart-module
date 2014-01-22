39f782c284c1fbd10df5c7d4eeb5a378f3f0a087
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-01-22 13:20:04 +0100

    some additional comments

--- src/includes/modules/payment/sveawebpay_invoice.php
+++ src/includes/modules/payment/sveawebpay_invoice.php
@@ -338,6 +338,7 @@ class sveawebpay_invoice extends SveaZencart {
            );
         }
 
+        // creates non-item order rows from Order Total entries
         $swp_order = $this->parseOrderTotals( $order_totals, $swp_order );
 
         // Check if customer is company
