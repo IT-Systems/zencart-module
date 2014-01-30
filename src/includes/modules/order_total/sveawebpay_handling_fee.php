795598433c4aa5e9f7aeb9910fdb28f86ba0d9ff
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-01-30 13:51:12 +0100

    Turned on ability to enable/disable invoice fee.

--- src/includes/modules/order_total/sveawebpay_handling_fee.php
+++ src/includes/modules/order_total/sveawebpay_handling_fee.php
@@ -41,6 +41,11 @@ class sveawebpay_handling_fee {
         if( $_SESSION['payment'] != "sveawebpay_invoice" ) {
             return;
         }
+
+        // only add to order total rows if the invoice fee is enabled
+        if( $this->enabled != "true" ) {
+            return;
+        }
         
         // get customer country from order
         $countryCode = $order->customer['country']['iso_code_2'];
@@ -85,9 +90,6 @@ class sveawebpay_handling_fee {
     
     // standard zencart module functions below
     
-    /**
-     * return true if module is enabled (i.e. installed)
-     */
     function check() {
         global $db;
         if (!isset($this->_check)) {
@@ -99,24 +101,25 @@ class sveawebpay_handling_fee {
 
     function install() {
         global $db;
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('This module is installed', 'MODULE_ORDER_TOTAL_SWPHANDLING_STATUS', 'true', '', '6', '1','zen_cfg_select_option(array(\'true\'), ', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, set_function) values ('Enable Svea Invoice Fee', 'MODULE_ORDER_TOTAL_SWPHANDLING_STATUS', 'True', 'Do you want to apply the Svea invoice fee?', '6', '0', now(), 'zen_cfg_select_option(array(\'True\', \'False\'), ')");        
+       
         $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_ORDER_TOTAL_SWPHANDLING_SORT_ORDER', '299', 'Sort order of display.', '6', '3', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (SE)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_SE"."', '20', 'Invoice fee will be applied to orders using the invoice payment method. Specify amount excluding tax, in shop default currency.', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (SE)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_SE"."', '0', 'Tax class for invoice fee.', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");     
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (NO)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_NO"."', '0.0', '', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (NO)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_NO"."', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (SE)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_SE', '20', 'Invoice fee will be applied to orders using the invoice payment method. Specify amount excluding tax, in shop default currency.', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (SE)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_SE', '0', 'Tax class for invoice fee.', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");     
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (NO)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_NO', '0.0', '', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (NO)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_NO', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");
 
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (DK)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_DK"."', '0.0', '', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (DK)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_DK"."', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (DK)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_DK', '0.0', '', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (DK)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_DK', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
 
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (FI)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_FI"."', '0.0', '', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (FI)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_FI"."', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (FI)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_FI', '0.0', '', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (FI)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_FI', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
 
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (NL)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_NL"."', '0.0', '', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (NL)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_NL"."', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (NL)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_NL', '0.0', '', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (NL)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_NL', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
 
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee"." (DE)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE"."_DE"."', '0.0', '', '6', '0', now())");
-        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class"." (DE)"."', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS"."_DE"."', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Fee (DE)', 'MODULE_ORDER_TOTAL_SWPHANDLING_HANDLING_FEE_DE', '0.0', '', '6', '0', now())");
+        $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Tax Class (DE)', 'MODULE_ORDER_TOTAL_SWPHANDLING_TAX_CLASS_DE', '0', '', '6', '0', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())");        
     }
 
     function remove() {
