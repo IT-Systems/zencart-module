5298282a16769d0e20b4825ff5dae9c18500bda5
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2014-02-17 10:16:30 +0100

    observer to get and print widget

---
+++ src/includes/auto_loaders/config.svea_product_price.php
@@ -0,0 +1,8 @@
+<?php
+$autoLoadConfig[190][] = array('autoType'=>'class',
+                              'loadFile'=>'observers/class.svea_product_price.php');
+$autoLoadConfig[190][] = array('autoType'=>'classInstantiate',
+                              'className'=>'svea_product_price',
+                              'objectName'=>'svea_product_price');
+ 
+?>
